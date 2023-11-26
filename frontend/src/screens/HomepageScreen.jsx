9a778ac0b888e0084b5b29cca3aace86824c962c
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2023-11-26 21:47:11 +0545

    comment remove

--- frontend/src/screens/HomepageScreen.jsx
+++ frontend/src/screens/HomepageScreen.jsx
@@ -1,39 +1,3 @@
-// // import React from 'react';
-// // import Navbar from '../components/Navbar';
-// // import Calendar from "react-calendar";
-// // import "./HomepageScreen.css"
-// // const HomepageScreen = () => {
-// //   return (
-// //     <div className="home-page">
-
-// //     <div>
-// //         <Navbar/>
-// //         {/* <Calendar/> */}
-// // </div>
-// // </div>
-
-// //   );
-// // };
-
-// // export default HomepageScreen ;
-
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-// import Calendar from "react-calendar";
-// import "./HomepageScreen.css";
-
-// const HomepageScreen = () => {
-//   return (
-//     <div className="home-page">
-//       <Navbar />
-//       <div className="calendar-container">
-//         <Calendar />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomepageScreen;
 
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
