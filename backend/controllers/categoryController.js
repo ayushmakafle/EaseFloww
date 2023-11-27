fb8d18255df82b9874a7ced773f1786b6c730cc0
ayushmakafle <amikafle1@gmail.com>
2023-11-27 22:51:02 +0545

    update category api

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -31,3 +31,29 @@ export const createCategoryController = async(req,res) => {
         })
     }
 };
+
+//update category
+export const updateCategoryController=async (req,res) => {
+    try{
+        const {name} = req.body
+        const {id} = req.params
+        const category = await CategoryModel.findByIdAndUpdate(
+            id,
+            {name,slug:slugify(name)},
+            {new:true},
+            )
+        res.status(200).send({
+            success:true,
+            message:"Category Updated Successfully",
+            category 
+        })
+
+    }catch(error){
+        res.status(500).send({
+            success:false,
+            error,
+            message:'error while updating category'
+        })
+    }
+
+}
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { requireSignIn,isAdmin } from "../middleware/authMiddleware.js";
-import { createCategoryController } from '../controllers/categoryController.js';
+import { createCategoryController,updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
 //routes
 router.post('/create-category',requireSignIn,isAdmin,createCategoryController)
+
+//update category
+router.put('/update-category/:id',requireSignIn,isAdmin,updateCategoryController)
 
 export default router
