802bae08e68dab28cb43af588851c246258b0e39
ayushmakafle <amikafle1@gmail.com>
2023-11-27 23:17:39 +0545

    category crud api

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -57,3 +57,59 @@ export const updateCategoryController=async (req,res) => {
     }
 
 }
+
+//get all categories
+export const getAllCategory = async(req,res) =>{
+    try{
+        const category = await CategoryModel.find({})
+        res.status(200).send({
+            success:true,
+            message:"all categories list",
+            category
+        })
+
+    }catch(error){
+        res.status(500).send({
+        success:false,
+        error,
+        message:'error getting categories'
+        })
+    }
+}
+
+//single category
+export const singleCategoryController = async(req,res) => {
+    try{
+        const category = await CategoryModel.findOne({slug:req.params.slug})
+        res.status(200).send({
+            success:true,
+            message:"get single category success",
+            category
+        })
+    }catch(error){
+    res.status(500).send({
+        success:false,
+        error,
+        message:'error getting category'
+        })
+    }
+}
+
+//delete category
+export const deleteCategoryController = async(req,res)=>{
+    try{
+        const { id } = req.params;
+        await CategoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success:true,
+            message:'category deleted'
+        })
+    }catch(error){            
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            error,
+            message:'error deleting category'
+        })
+    }
+}
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { requireSignIn,isAdmin } from "../middleware/authMiddleware.js";
-import { createCategoryController,updateCategoryController } from '../controllers/categoryController.js';
+import { createCategoryController,getAllCategory,singleCategoryController,updateCategoryController,deleteCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
@@ -9,5 +9,14 @@ router.post('/create-category',requireSignIn,isAdmin,createCategoryController)
 
 //update category
 router.put('/update-category/:id',requireSignIn,isAdmin,updateCategoryController)
+
+//get all category
+router.get('/get-category',getAllCategory)
+
+//single category
+router.get('/single-category/:slug',singleCategoryController)
+
+//delete category
+router.delete('/delete-category/:id',requireSignIn,isAdmin,deleteCategoryController)
 
 export default router
