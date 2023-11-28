fc93021f936971412fa2a68049a3b732573383f1
ayushmakafle <amikafle1@gmail.com>
2023-11-28 18:05:46 +0545

    Update category

--- frontend/src/Admin/CreateCategory.js
+++ frontend/src/Admin/CreateCategory.js
@@ -4,11 +4,15 @@ import AdminMenu from './AdminMenu'
 import { toast } from 'react-toastify'
 import axios from 'axios'
 import CategoryForm from '../components/Form/CategoryForm'
+import {Modal} from 'antd'
 
 const CreateCategory = () => {
 
   const [categories,setCategories] = useState([])
   const [name,setName]= useState("")
+  const [visible, setVisible] = useState(false);//for pop up modal
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   //handle form
   const handleSubmit = async(e) =>{
@@ -45,6 +49,46 @@ const CreateCategory = () => {
     getAllCategory()
   },[])
 
+  //update category
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        toast.success(`${updatedName} is updated`);
+        setSelected(null);
+        setUpdatedName("");
+        setVisible(false);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  //delete category
+  const handleDelete = async (pId) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/category/delete-category/${pId}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        toast.success(`category is deleted`);
+        setUpdatedName("");
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Somtihing went wrong");
+    }
+  };
 
   return (
      <>
@@ -75,14 +119,26 @@ const CreateCategory = () => {
                       <td style={{ textAlign: 'center' }}>{index + 1}</td>
                       <td style={{ textAlign: 'left' }}>{c.name}</td>
                       <td>
-                        <button className='btn btn-dark ms-2' style={{ background: '#FF5FA3' }}>Edit</button>
-                        <button className='btn btn-dark ms-2' style={{ background: '#FF06BF ' }}>Delete</button>
+                        <button className='btn btn-dark ms-2' style={{ background: '#FF5FA3'}}
+                          onClick={() => {setVisible(true) ;
+                          setUpdatedName(c.name);
+                          setSelected(c)}}>
+                          Edit</button>
+                        <button className='btn btn-dark ms-2' style={{ background: '#FF06BF ' }}
+                          onClick={() => {handleDelete(c._id)}}>
+                          Delete</button>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
+            <Modal onCancel={()=>setVisible(false)} footer={null} open={visible}>
+              <CategoryForm 
+                value={updatedName} 
+                setValue={setUpdatedName} 
+                handleSubmit={handleUpdate}/>
+            </Modal>
           </div>
         </div>
       </div>
