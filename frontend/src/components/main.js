2ed5a1ad599405e76b1d56e51220865117ab8259
ayushmakafle <amikafle1@gmail.com>
2023-12-03 21:20:07 +0545

    deleted

--- frontend/src/components/main.js
+++
@@ -1,8 +0,0 @@
-const Navbar =document.querySelector("Navbar");
-window.addEventListener("scroll",() => {
-    if(window.pageYOffset>60){
-        Navbar.classList.add("scrolled");
-    }else{
-        Navbar.classList.remove("scrolled");
-    }
-})
