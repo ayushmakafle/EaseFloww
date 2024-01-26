1cbbfd95626ea61d9cf564e14981b0b4a987d840
ayushmakafle <amikafle1@gmail.com>
2024-01-26 15:27:40 +0545

    doctor animation

--- frontend/src/screens/AllDoctors.js
+++ frontend/src/screens/AllDoctors.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 // import MainNavbar from '../components/Navbar';
 import DoctorCard from '../components/DoctorCard';
 import axios from 'axios';
+import animation from '../components/finddoctor.json';
+import Lottie from 'lottie-react';
 
 const AllDoctors = () => {
   const [doctors, setDoctors] = useState([]);
@@ -26,22 +28,36 @@ const AllDoctors = () => {
   }, []); // Run this effect only once on component mount
 
   return (
-    <>
-      {/* <MainNavbar /> */}
-      <h1 style={{ textAlign: 'center', color: '#ef5e99', marginTop: '20px' }}>
-        EaseFlow Doctors
-      </h1>
-
-      <div className="doctor-cards-container" 
-        style={{ cursor: 'pointer', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
-      >
-        {doctors.map((doctor) => (
-          <Link key={doctor._id} to={`/book-appointment/${doctor._id}`} style={{textDecoration:'none'}}>
-            <DoctorCard doctor={doctor} />
-          </Link>
-        ))}
+ <>
+    {/* <MainNavbar /> */}
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: '1', maxWidth: '20%', marginTop:'40px' }}>
+        <Lottie
+          animationData={animation}
+          className="lottie-animation-home cursor-pointer"
+        />
+        <h4 style={{ textAlign: 'center', color: '#ff0066', marginTop: '20px' }}>
+          Discover EaseFlow: Your Trusted Path to Wellness – Find the Perfect Doctor Today
+        </h4>
       </div>
-    </>
+      
+      <div style={{ flex: '1' }}>
+        <h1 style={{ textAlign: 'center', color: '#ef5e99', marginTop: '20px' }}>
+          EaseFlow Doctors
+        </h1>
+
+        <div className="doctor-cards-container" 
+          style={{ cursor: 'pointer', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+        >
+          {doctors.map((doctor) => (
+            <Link key={doctor._id} to={`/book-appointment/${doctor._id}`} style={{textDecoration:'none'}}>
+              <DoctorCard doctor={doctor} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  </>
   );
 };
 
