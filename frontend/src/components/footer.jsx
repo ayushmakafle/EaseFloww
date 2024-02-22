4221492ae520b4dedca7394ef6eafe582be5ecff
ayushmakafle <amikafle1@gmail.com>
2024-02-23 00:03:58 +0545

    footer update

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,57 +4,54 @@ import '../styles/MainNavbar.css';
 const MainFooter = () => {
     return (
         <>
-    <footer>
-        <div class="wrapper">
-            <div class="links-container">
-                <div class="links">
-                    <h3>Quick Links</h3>
-                    <ul>
-                        <li>
-                            <a href="#">About us</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact us</a>
-                        </li>
-                        <li>
-                            <a href="#">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#">Terms and Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="links">
-                    <h3>Encyclopedia</h3>
-                    <ul>
-                        <li>
-                            <a href="/Login">Login</a>
-                        </li>
-                        <li>
-                            <a href="/queries">Chatbot</a>
-                        </li>
-                        <li>
-                            <a href="/appointment-homepage">Gynecologist appointment</a>
-                        </li>
-                        <li>
-                            <a href="/ecommerce">Our Products</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="links">
-                    <h3>Contact Us</h3>
-                    <ul>
-                        <li>easeflow@gmail.com</li>
-                    </ul>
-                    <form action="#">
+            <footer>
+                <div class="wrapper">
+                    <div class="links-container">
+                        <div class="links">
+                            <h3>Quick Links</h3>
+                            <ul>
+                                <li>
+                                    <a href="/whyshophere">About us</a>
+                                </li>
+                                {/*   <li>
+                                    <a href="#">Contact us</a>
+                                </li> */}
+                                <li>
+                                    <a href="#">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="#">Terms and Conditions</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="links">
+                            <h3>Encyclopedia</h3>
+                            <ul>
+                                <li>
+                                    <a href="/Login">Login</a>
+                                </li>
+                                <li>
+                                    <a href="/appointment-homepage">Gynecologist appointment</a>
+                                </li>
+                                <li>
+                                    <a href="/ecommerce">Our Products</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="links">
+                            <h3>Contact Us</h3>
+                            <ul>
+                                <li>easeflow2023@gmail.com</li>
+                            </ul>
+                            {/* <form action="#">
                         <input type="text" placeholder="Email Address"/>
                         <button class="submit-btn">Subscribe</button>
-                    </form>
+                    </form> */}
+                        </div>
+                    </div>
+                    <p class="copyright text-center">© EaseFlow 2023</p>
                 </div>
-            </div>
-            <p class="copyright">© 2023 EaseFlow</p>
-        </div>
-    </footer>
+            </footer>
         </>
     );
 };
