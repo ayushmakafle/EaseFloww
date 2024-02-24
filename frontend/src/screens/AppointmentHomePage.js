32efa2d07c79c41d4954e56eb69be91029d67a0d
eurikabaral <eurikabaral@gmail.com>
2024-02-24 18:31:11 +0545

    color changes

--- frontend/src/screens/AppointmentHomePage.js
+++ frontend/src/screens/AppointmentHomePage.js
@@ -13,7 +13,7 @@ const AppointmentHomePage = () => {
     <div className="hero-section" style={{ overflowY: 'hidden' }}>
           <div className="left">
             <h2 style={{ fontFamily: 'Raleway, sans-serif', fontWeight: "600" ,fontSize:"60px", color:"white" , animation: "fadeIn 1s "}}>Hassle free appointments <br /> Just book online</h2>
-            <Nav.Link as={Link} to="/all-doctors" className="btn light">View EaseFlow Doctors</Nav.Link>
+            <Nav.Link as={Link} to="/all-doctors" className="btn light"style={{ fontFamily: 'Raleway, sans-serif', fontWeight: "45" ,fontSize:"20px", color:"white",backgroundColor:"#e73d90" }}>View EaseFlow Doctors</Nav.Link>
           </div>
           <div className="right">
             <img src="images/calenderbg.jpg" alt="" />
