e0e8ec7544dfa4aa3d7bb01a95c908a484951e95
eurikabaral <eurikabaral@gmail.com>
2024-03-07 10:26:48 +0545

    admin panel button text

--- frontend/src/Admin/AdminDashboard.js
+++ frontend/src/Admin/AdminDashboard.js
@@ -16,8 +16,8 @@ const AdminDashboard = () => {
         </div>
         <div className='col-md-9'>
           <div className='card w-75 p-3 m-5'>
-            <h1 style={{color:'#ef5e99',fontWeight: 'bold'}}>Welcome {auth?.user?.username}!</h1>
-            <h4 style={{color:'#f38dbc'}}>You've arrived at the heart of Easeflow's admin panel. </h4>
+            <h1 style={{color:'#ef5e99',fontWeight: 'bold', textShadow: '1px 1px 1px #f38dbc'}}>Welcome {auth?.user?.username}!</h1>
+            <h4 style={{color:'#ef5e99'}}>You've arrived at the heart of Easeflow's admin panel. </h4>
           </div>
         </div>
 
--- frontend/src/Admin/AdminMenu.js
+++ frontend/src/Admin/AdminMenu.js
@@ -4,30 +4,30 @@ import { NavLink } from 'react-router-dom';
 const AdminMenu = () => {
   return (
     <div className='text-center'>
-      <h4 style={{color:'#ef5e99', fontWeight:'bold'}}>Admin Panel</h4>
+      <h4 style={{fontFamily: 'Poppins, sans-serif',color:'#ef5e99',fontWeight:'600'}}>Admin Panel</h4>
       <div className="list-group">
-        <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           Create Category
         </NavLink>
-        <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           Insert Products
         </NavLink>
-        <NavLink to="/dashboard/admin/products" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/products" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           Products
         </NavLink>
-        <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           EaseFlow Users
         </NavLink>
-        <NavLink to="/dashboard/admin/doctors" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/doctors" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           EaseFlow Doctors
         </NavLink>
-        <NavLink to="/dashboard/admin/doctorapproval" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/doctorapproval" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           Approve New Doctors
         </NavLink>
-        <NavLink to="/dashboard/admin/order" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/order" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
         EaseFlow Orders  
         </NavLink>
-        <NavLink to="/dashboard/admin/appointments" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99'}}>
+        <NavLink to="/dashboard/admin/appointments" className="list-group-item list-group-item-action" style={{backgroundColor:'#ef5e99',color:'white'}}>
           Appointments List
         </NavLink>
       </div>
